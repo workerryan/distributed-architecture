ddf472f9ec128d361db60755161dd402777e9454
wanglei <workerryan@126.com>
2019-08-26 20:30:02 +0800

    增加了jta工程

---
+++ spring-boot/spring-boot-multi-datasourse-jta-atomikos/src/main/java/com/dragon/springboot/jta/JtaApplication.java
@@ -0,0 +1,12 @@
+package com.dragon.springboot.jta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JtaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JtaApplication.class, args);
+    }
+}
