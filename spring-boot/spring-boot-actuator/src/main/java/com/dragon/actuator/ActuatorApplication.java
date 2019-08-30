e0a5ce79b06528814a547694ac816da7e25b4c26
root <wangl@xiangxue.com.cn>
2019-08-30 21:45:26 +0800

    添加了actuator

---
+++ spring-boot/spring-boot-actuator/src/main/java/com/dragon/actuator/ActuatorApplication.java
@@ -0,0 +1,15 @@
+package com.dragon.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanglei
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
