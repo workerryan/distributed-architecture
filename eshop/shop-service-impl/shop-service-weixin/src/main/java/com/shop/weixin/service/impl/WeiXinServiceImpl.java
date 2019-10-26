6b88e3f55128d1e43d8537a0b3c2da9f843ef792
wangl <wangl@qq.com>
2019-10-26 17:09:30 +0800

    增加了Apollo集成

--- eshop/shop-service-impl/shop-service-weixin/src/main/java/com/shop/weixin/service/impl/WeiXinServiceImpl.java
+++ eshop/shop-service-impl/shop-service-weixin/src/main/java/com/shop/weixin/service/impl/WeiXinServiceImpl.java
@@ -2,15 +2,17 @@ package com.shop.weixin.service.impl;
 
 import com.shop.entity.AppEntity;
 import com.shop.weixin.service.WeiXinService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
-
 
 @RestController
 public class WeiXinServiceImpl implements WeiXinService {
+
+    @Value("${weixin.self.value}")
+    private String selfValue;
+
     @Override
     public AppEntity getApp() {
-        return new AppEntity("dragon", UUID.randomUUID().toString());
+        return new AppEntity("dragon", selfValue);
     }
 }
